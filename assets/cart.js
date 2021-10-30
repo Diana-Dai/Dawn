83fbbcc5206310594ee4cb0a60258a92c0241902
Diana Dai <dianadai@DianatekiMacBook-Air.local>
2021-10-30 12:17:23 +1100

    fixed bugs

--- assets/cart.js
+++ assets/cart.js
@@ -75,6 +75,7 @@ class CartItems extends HTMLElement {
         this.classList.toggle('is-empty', parsedState.item_count === 0);
         const cartFooter = document.getElementById('main-cart-footer');
 
+        s
         if (cartFooter) cartFooter.classList.toggle('is-empty', parsedState.item_count === 0);
 
         this.getSectionsToRender().forEach((section => {
